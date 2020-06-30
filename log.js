d6645f10ec03c2634cfbc02384d5fc76039a448f
Brent Groves <brent.groves@gmail.com>
2020-06-30 11:56:54 -0400

    Added/Changed Code

--- log.js
+++ log.js
@@ -1,6 +1,22 @@
 function log (...args) {
   if (process.env.NODE_ENV === "development") {
     console.log(...args);
+   // console.trace();
+   try {
+      throw new Error();
+    } catch (error) {
+      console.log(
+    error
+    .stack // Grabs the stack trace
+    .split('\n')[2] // Grabs third line
+    .trim() // Removes spaces
+    .substring(3) // Removes three first characters ("at ")
+//    .replace(__dirname, '') // Removes script folder path
+    .replace(/\s\(./, ' at ') // Removes first parentheses and replaces it with " at "
+    .replace(/\)/, '') // Removes last parentheses
+      )
+    }
+
   }
 }
 
